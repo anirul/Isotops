9227cd3a223c4f3959dea0ce7708f89836b0c0ea
Frederic Dubouchet (anirul) <frederic.dubouchet@gmail.com>
2016-03-26 21:56:29 +0100

    Started implementing text name and description
    Name and description of the element

--- Source/Isotops/NucleusModel.h
+++ Source/Isotops/NucleusModel.h
@@ -5,59 +5,54 @@
 
 USTRUCT(BlueprintType)
 struct FNucleon {
-	GENERATED_USTRUCT_BODY()
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	FString Symbol;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	FString Element;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	FString Description;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	FString Symbol;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float HalfLife;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float Life;
+  GENERATED_USTRUCT_BODY()
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  FString Symbol;
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  FString Element;
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  FString Description;
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  float HalfLife;
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  float Life;
 };
 
 UENUM(BlueprintType)
 enum class EDecayType : uint8 {
-	Bplus UMETA(DisplayName="Beta+"),
-	Bminus UMETA(DisplayName="Beta-"),
-	EC UMETA(DisplayName="Electron capture"),
-	Eplus UMETA(DisplayName="e+???"),
-	Nucleon UMETA(DisplayName="Nucleon ejection"),
+  Bplus UMETA(DisplayName = "Beta+"),
+  Bminus UMETA(DisplayName = "Beta-"),
+  EC UMETA(DisplayName = "Electron capture"),
+  Eplus UMETA(DisplayName = "e+???"),
+  Nucleon UMETA(DisplayName = "Nucleon ejection"),
 };
 
 USTRUCT(BlueprintType)
 struct FDecayMode {
-	GENERATED_USTRUCT_BODY()
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	EDecayType Type;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	int32 Protons;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	int32 Neutrons;
+  GENERATED_USTRUCT_BODY()
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  EDecayType Type;
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  int32 Protons;
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  int32 Neutrons;
 };
 
 UCLASS()
 class ISOTOPS_API UNucleusModel : public UBlueprintFunctionLibrary {
-	GENERATED_BODY()
+  GENERATED_BODY()
 public:
-	
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category=NucleusModel)
-	static FNucleon Create(int32 Protons, int32 Neutrons, float Random);
-	
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category=NucleusModel)
-	static TArray<FDecayMode> Decay(int32 Protons, int32 Neutrons, float Random);
-	
+  UFUNCTION(BlueprintCallable, BlueprintPure, Category = NucleusModel)
+  static FNucleon Create(int32 Protons, int32 Neutrons, float Random);
+
+  UFUNCTION(BlueprintCallable, BlueprintPure, Category = NucleusModel)
+  static TArray<FDecayMode> Decay(int32 Protons, int32 Neutrons, float Random);
 };
